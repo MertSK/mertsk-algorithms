4d17c9927b2036e20b06dd018989f99c1edfc449
Mert ŞIK <mertsk9091@gmail.com>
2022-11-27 16:57:53 +0300

    algorithm-cs

---
+++ Algorithms with C#/P008_SumofNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P008_SumofNumbers
+
+{
+
+    /* 
+        girilen sayilarin toplamini bulma
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sum = 0;
+
+            do
+            {
+                Console.WriteLine("Please enter a number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                if (number != -1)
+                {
+                    sum += number;
+                }
+            }
+            while(number != -1);
+
+            Console.WriteLine("Sum of numbers that you entered: " + sum);
+
+        }
+    }
+}
